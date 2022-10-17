99b9972dd0f49e36afc0eab2a9fb11be0c9ce2b3
ASZhartun <as.zhartun@gmail.com>
2022-10-17 15:03:19 +0300

    feature: add part of functionality of binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,49 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.ancient = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.ancient;
+  }
+
+  add(data) {
+    let currentNode = new Node(data);
+    if (this.ancient === null) {
+      this.ancient = currentNode;
+    } else {
+      this.addNodeOnSubTree(this.ancient, currentNode);
+    }
+  }
+
+  addNodeOnSubTree(root, node) {
+    if (node.data < root.data) {
+      if (root.left === null) root.left = node;
+      else this.addNodeOnSubTree(root.left, node);
+    } else if (node.data > root.data) {
+      if (root.right == null) root.right = node;
+      else this.addNodeOnSubTree(root.right, node);
+    }
+  }
+
+  move(node, action, inputNode) {
+    if (node!==null) {
+      this.move(node.left, action, inputNode);
+      action(node);
+      this.move(node.right, action, inputNode);
+    }
+  }
+
+  action(node) {
+    console.log(`current node: ${node.data}`);
   }
 
   has(/* data */) {
@@ -23,9 +51,25 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let searchingItem = new Node(data);
+    if (this.ancient === null) {
+      return null;
+    } else {
+      return this.findSpecifiedItem(this.ancient, searchingItem);
+    }
+  }
+
+  findSpecifiedItem(currentNode, searchingNode) {
+    if (currentNode !== null) {
+      if (searchingNode.data > currentNode.data) {
+        return this.findSpecifiedItem(currentNode.right, searchingNode);
+      } else if (searchingNode.data < currentNode.data) {
+        return this.findSpecifiedItem(currentNode.left, searchingNode);
+      } else if (searchingNode.data == currentNode.data){
+        return currentNode;
+      }
+    } else return null;
   }
 
   remove(/* data */) {
